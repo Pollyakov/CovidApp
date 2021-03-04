9ef72fb6631ea49e8b8e0b4b1ee9d29b5bfc55cf
Ksenia Gountsova <ksusha.net@gmail.com>
2021-03-04 16:56:39 +0200

    initial

---
+++ Weekend_assignments/Covid_App/script.js
@@ -0,0 +1,39 @@
+const world = [];
+//url with proxy
+const AllCountriesURL =
+  `https://api.codetabs.com/v1/proxy?quest=` +
+  `https://restcountries.herokuapp.com/api/v1
+  `;
+const getWorld = async () => {
+  const allResp = await fetch(AllCountriesURL);
+  const allJson = await allResp.json();
+  console.log(allJson[0].cca2);
+  // e- eto object Afganistana 
+  // allJson.forEach((e) => {
+  //   if (!world[e.region]) {
+  //     world[e.region] = [e];
+  //   } else {world[e.region].push(e);}  });
+  world = allJson.map( (c) => {cca2: c[cca2],
+                             region: c[region],
+                            
+});
+};
+getWorld();
+const stats = [];
+let code = "AF";
+const url = "https://api.codetabs.com/v1/proxy?quest=" +
+`http://corona-api.com/countries/${code}`;
+
+const getStats = async () => {
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    console.log("Covid stats ", jsonResponse);
+};
+
+getStats();
+
+
+
+
+
+
