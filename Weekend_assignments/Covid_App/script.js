8cb0e62ab9593797a9f92712771167ff8fd954ea
Ksenia Gountsova <ksusha.net@gmail.com>
2021-03-06 23:08:02 +0200

    displayCountries

--- Weekend_assignments/Covid_App/script.js
+++ Weekend_assignments/Covid_App/script.js
@@ -10,7 +10,6 @@ const getWorld = async () => {
     return { cca2: c.cca2, region: c.region, name: c.name.common };
   });
   console.log("World", world);
-
 };
 //getting world object with countries per continent onLoad
 document.addEventListener("DOMContentLoaded", function () {
@@ -18,29 +17,48 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 //
 const getCountries = (e) => {
-  const continent = world.filter((c) => { 
+  const continent = world.filter((c) => {
     return c["region"] == e.path[0].textContent;
-    }
-  );
+  });
   console.log("Continent array ", continent);
-}; 
+  let countries = continent.map((c) => {
+    return c["name"];
+  });
+  console.log(`Countries of ${e.path[0].textContent} are: ${countries}`);
+  displayList(countries);
+};
+const displayList = (countries) => {
+  // const target = document.querySelector(".container");
+  // const olEl = document.createElement("ol");
+  // for (const c of countries) {
+  //   let countrList.innerHTML += `<div>${c}</div>`;
+  // }
+  let mytable = "<table><tr>";
+  for (let name of countries) {
+    mytable += "<td>" + name + "</td>";
+  }
+  mytable += "</tr></table>";
+  countryList.innerHTML = mytable;
+};
+
 let btns = document.querySelector(".btns");
+let countryList = document.querySelector(".countries");
 btns.addEventListener("click", (e) => getCountries(e));
 
 //COVID API
-// const stats = [];
-// let code = "AF";
-// const url =
-//   "https://api.codetabs.com/v1/proxy?quest=" +
-//   `http://corona-api.com/countries/${code}`;
+const stats = [];
+let code = "AF";
+const url2 =
+  "https://api.codetabs.com/v1/proxy?quest=" +
+  `http://corona-api.com/countries/${code}`;
 
-// const url =
-//   "https://api.codetabs.com/v1/proxy?quest=" +
-//   `http://corona-api.com/countries`;
+const url =
+  "https://api.codetabs.com/v1/proxy?quest=" +
+  `http://corona-api.com/countries`;
 
-// const getStats = async () => {
-//   const response = await fetch(url);
-//   const jsonResponse = await response.json();
-//   console.log("Covid stats ", jsonResponse);
-// };
-// getStats();
+const getStats = async () => {
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+  console.log("Covid stats ", jsonResponse);
+};
+getStats();
